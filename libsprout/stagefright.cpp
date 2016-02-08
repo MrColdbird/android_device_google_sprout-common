f1476242f8b891c8058626d4e5c2ceba455958bb
MrColdbird <gentleman.trilby@gmail.com>
2016-02-08 20:43:08 +0100

    Updated stagefright shim

--- libsprout/stagefright.cpp
+++ libsprout/stagefright.cpp
@@ -3,3 +3,8 @@ extern "C" int _ZN7android13AStringPrintfEPKcz();
 extern "C" int _ZN7android12StringPrintfEPKcz() {
     return _ZN7android13AStringPrintfEPKcz();
 }
+
+extern "C" int _ZN7android8AMessageC1Ev();
+extern "C" int _ZN7android8AMessageC1Eji() {
+    return _ZN7android8AMessageC1Ev();
+}
