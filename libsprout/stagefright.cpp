0e61fa4dbbf4844331eb4b5d15bd6f81b1fcd0f9
MrColdbird <gentleman.trilby@gmail.com>
2016-02-10 00:05:32 +0100

    Revert "Updated stagefright shim"
    This reverts commit f1476242f8b891c8058626d4e5c2ceba455958bb.

--- libsprout/stagefright.cpp
+++ libsprout/stagefright.cpp
@@ -3,8 +3,3 @@ extern "C" int _ZN7android13AStringPrintfEPKcz();
 extern "C" int _ZN7android12StringPrintfEPKcz() {
     return _ZN7android13AStringPrintfEPKcz();
 }
-
-extern "C" int _ZN7android8AMessageC1Ev();
-extern "C" int _ZN7android8AMessageC1Eji() {
-    return _ZN7android8AMessageC1Ev();
-}
