5ff07fdba47e7de5666144f349d81477bb164a97
MrColdbird <gentleman.trilby@gmail.com>
2016-02-10 00:03:38 +0100

    Revert "I hate myself for this"
    This reverts commit 1b15721dbea93d187c63b61d0481e3dd141fd07f.

--- libsprout/audioflinger.cpp
+++ libsprout/audioflinger.cpp
@@ -9,9 +9,3 @@ extern "C" int _ZN7android11AudioSystem15SetAudioCommandEii() {
 extern "C" int _ZN7android11AudioSystem15GetAudioCommandEiPi() {
     return 0;
 }
-
-extern "C" int _ZN7android13MediaRecorderC1ERKNS_8String16E();
-extern "C" int _ZN7android13MediaRecorderC1Ev() {
-    // this will hurt us so bad...
-    return _ZN7android13MediaRecorderC1ERKNS_8String16E();
-}
